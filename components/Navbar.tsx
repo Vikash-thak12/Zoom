14a416a07940e1f3a9d8dabad8b66d5d97fc6d8d
Vikash Thakur <thakvikash622@gmail.com>
2024-11-18 00:39:39 +0530

    Updated: Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,11 +8,11 @@ const Navbar = () => {
   return (
     <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
       <Link href={"/"} className='flex items-center gap-2'>
-        <Image src={"/icons/logo.svg"} height={32} width={32} alt='Main logo' className='size-14 max-sm:size-10' />
+        <Image src={"/icons/logo.svg"} height={24} width={24} alt='Main logo' className='size-14 max-sm:size-10' />
         <p className='text-white font-bold'>Zoom</p>
       </Link>
 
-      <div>
+      <div className='flex gap-2'>
         <SignedOut>
           <SignInButton />
         </SignedOut>
@@ -21,9 +21,9 @@ const Navbar = () => {
         <SignedIn>
           <UserButton />
         </SignedIn>
-      </div>
 
       <MobileNav />
+      </div>
     </nav>
   )
 }
