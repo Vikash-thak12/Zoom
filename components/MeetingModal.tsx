38ea036bbaf18f570b37842275bb8031f95ca980
Vikash Thakur <thakvikash622@gmail.com>
2024-11-21 10:08:40 +0530

    Installed: Shadcn Dialog

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,24 +1,35 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { ReactNode } from 'react'
 
-interface MeetingModelProps{
-    isopen: boolean, 
-    onclose: () => void, 
-    className?: string, 
-    title: string, 
-    children?: ReactNode, 
-    handleClick: () => void, 
-    buttonText?: string, 
-    image: string, 
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+interface MeetingModelProps {
+    isopen: boolean,
+    onclose: () => void,
+    className?: string,
+    title: string,
+    children?: ReactNode,
+    handleClick: () => void,
+    buttonText?: string,
+    image: string,
     buttonIcon?: string
 }
 
-const MeetingModal = ({ isopen, onclose, className, title, children, handleClick, buttonText, image, buttonIcon}: MeetingModelProps) => {
-  return (
-    <div>
-      Meeting Modal
-    </div>
-  )
+const MeetingModal = ({ isopen, onclose, className, title, children, handleClick, buttonText, image, buttonIcon }: MeetingModelProps) => {
+    return (
+        <Dialog open={isopen} onOpenChange={onclose}>
+            <DialogContent>
+
+            </DialogContent>
+        </Dialog>
+    )
 }
 
 export default MeetingModal
