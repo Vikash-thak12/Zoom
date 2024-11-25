b6ad155316299655ea45b7a3c43ca082d37be463
Vikash Thakur <thakvikash622@gmail.com>
2024-11-25 14:03:22 +0530

    Creating: Recording page

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,9 +1,12 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const RecodingPage = () => {
   return (
-    <div>
-      <h1>Recording page</h1>
+    <div className='text-white flex flex-col gap-8'>
+      <h1 className='text-3xl font-bold'>Recording page</h1>
+
+      <CallList type='recordings' />
     </div>
   )
 }
