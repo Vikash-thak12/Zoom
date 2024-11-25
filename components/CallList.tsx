24c4ade39c2d5324d854c73e60960d62f8713a14
Vikash Thakur <thakvikash622@gmail.com>
2024-11-25 14:22:04 +0530

    Fixed: Recordings section

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -3,9 +3,10 @@
 import { useGetCalls } from '@/hooks/useGetCalls'
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MeetingCard from './MeetingCard';
 import Loader from './Loader';
+import { toast } from '@/hooks/use-toast';
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
     const { endedCalls, upcomingCalls, callrecordings, isLoading } = useGetCalls();
@@ -39,6 +40,23 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
         }
     }
 
+    useEffect(() => {
+        const fetchRecordings = async () => {
+            try {
+                const callData = await Promise.all(callrecordings?.map((meeting) => meeting.queryRecordings()) ?? [])
+
+                const recordings = callData
+                    .filter((call) => call.recordings.length > 0)
+                    .flatMap((call) => call.recordings)
+
+                setRecordings(recordings)
+            } catch (error) {
+                toast({ title: "Try Again Later" })
+            }
+        }
+        if (type === "recordings") fetchRecordings();
+    }, [type, callrecordings])
+
     const calls = getCalls();
     const nocallmessage = getNoCallsMessage()
 
@@ -47,16 +65,16 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
         <div className='grid grid-cols-1 xl:grid-cols-2 gap-5'>
             {calls && calls.length > 0 ? calls.map((meeting: Call | CallRecording) => (
                 <MeetingCard
-                    key={(meeting as Call).id}
+                    key={(meeting as Call)?.id}
                     icon={
-                        type === "ended" 
-                        ? '/icons/previous.svg' 
-                        : type === "upcoming" 
-                        ? '/icons/upcoming.svg' 
-                        : '/icons/recordings.svg'
+                        type === "ended"
+                            ? '/icons/previous.svg'
+                            : type === "upcoming"
+                                ? '/icons/upcoming.svg'
+                                : '/icons/recordings.svg'
                     }
-                    title={(meeting as Call).state.custom.description || "No Description for this meeting"}
-                    
+                    title={(meeting as Call).state?.custom?.description || meeting.filename.substring(0,20) || "No Description for this meeting"}
+
                     // I was getting issue in the date bc it shoud be startsAt instead of startedAt
                     date={(meeting as Call).state?.startsAt?.toLocaleString() || (meeting as CallRecording).start_time?.toLocaleString()}
                     isPreviousMeeting={type === "ended"}
