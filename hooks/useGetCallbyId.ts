a0306f13e621ea2df5c63e59fe3f2d777c35d50c
Vikash Thakur <thakvikash622@gmail.com>
2024-12-18 22:35:47 +0530

    Minor changes

--- hooks/useGetCallbyId.ts
+++ hooks/useGetCallbyId.ts
@@ -8,13 +8,13 @@ export const useGetCallbyId  = (id: string | string[]) => {
     const [isCallLoading, setIsCallLoading] = useState(true)
 
     const client = useStreamVideoClient();
+    
 
     useEffect(() => {
         if(!client) return; 
         const loadcall = async () => {
             try {
                 const { calls } = await client.queryCalls({ filter_conditions: { id }})
-                console.log("LoadCall", calls[0])
                 if(calls.length > 0) setCall(calls[0])
                 setIsCallLoading(false)
             } catch (error) {
